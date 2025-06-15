e7e419759eb173951619fa68afa135796286bed2
Hakan Akcinar <hakcinar1@gmail.com>
2025-06-15 17:51:56 +0300

    Task-1 Task-3 Changes

--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,7 @@ function getElementWidth(content, padding, border) {
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
   const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-  const checkedWidth = Math.round(totalWidth);
-  return totalWidth;
+  return Math.round(totalWidth);
 }
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
